d623670612f02e1a7e130c421a32d942f0d733f4
nhatminh287 <nhatminhpro2001@gmail.com>
2024-04-01 00:37:46 +0700

    fix font-size footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
     return (
-        <div className="w-full h-20 mb-[2.5rem]">
+        <div className="w-full h-20 ">
             <p className="w-[60rem] text-gray-400 w-[50rem] ml-[50%] translate-x-[-50%] mb-[0.5rem] text-xs pt-[1rem]">
                 This website is createed as part of Hlsolutions program. The materials contained on this website
                 are provided for general information only and do not constitute any form of advice. HLS assumes no
                 responsibility for the accuracy of any particular statement and accepts no liability for any loss or damage
                 which may arise from reliance on the information contained on this site.
             </p>
-            <p className="">Copyright 2021 HLS</p>
+            <p className="text-sm">Copyright 2021 HLS</p>
         </div>
     )
 }
