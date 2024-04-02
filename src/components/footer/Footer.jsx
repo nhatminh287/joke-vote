3abab9c8841ee0fa956d47de8e739e5486cf2cf8
nhatminh287 <nhatminhpro2001@gmail.com>
2024-04-02 15:27:52 +0700

    fix bug

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="w-full md:h-20 sm:h-24 h-36">
-            <p className="lg:w-[60rem] md:w-[45rem] sm:w-[35rem] w-[19rem] text-gray-400 w-[50rem] ml-[50%] translate-x-[-50%] mb-[0.5rem] text-xs pt-[1rem]">
+            <p className="lg:w-[60rem] md:w-[45rem] sm:w-[35rem] w-[19rem] text-gray-400 ml-[50%] translate-x-[-50%] mb-[0.5rem] text-xs pt-[1rem]">
                 This website is createed as part of Hlsolutions program. The materials contained on this website
                 are provided for general information only and do not constitute any form of advice. HLS assumes no
                 responsibility for the accuracy of any particular statement and accepts no liability for any loss or damage
